11168294a74ba69e682da8e6d8113ae5ba6efb60
SILVIO DIAS <sdferreira@sad.ms.gov.br>
2024-11-19 13:55:33 -0400

    aula 20 - props e organização

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,11 @@
+export const PostCard = ({ cover, title, id, body }) => (
+  <div className="posts">
+    <div className="post">
+      <img src={cover} alt={title} />
+      <div key={id} className="post-content">
+        <h1>{title}</h1>
+        <p>{body}</p>
+      </div>
+    </div>
+  </div>
+);
